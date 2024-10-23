029770b8e51aa671f05181ab1f4d6529c77b08b8
development-hobby <hobbydevelopmentteam@gmail.com>
2024-10-23 18:35:31 -0400

    feat: add analytics next.js and optimize performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
